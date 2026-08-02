3a17407c03748b45dde93deff4ab537ff3e5dbad
Aman Pal <amanpal2321@gmail.com>
2026-08-02 22:12:02 +0530

    Day2: Print  Revere Array

---
+++ Program7.java
@@ -0,0 +1,9 @@
+//Reverse Array?
+public class Program7 {
+    public static void main(String[] args){
+        int[] arr={4,8,62,4,6,1};
+
+        for(int i=arr.length-1;i>=0;i--)
+        System.out.println( arr[i]);
+    }
+}
