1b85a8e1b8d84c3b322759cc5bb0091292e13509
Aman Pal <amanpal2321@gmail.com>
2026-08-03 20:49:28 +0530

    Day 3: Reverse the array

---
+++ Program10.java
@@ -0,0 +1,11 @@
+// Reverse the Array?
+public class Program10 {
+    public static void main(String[] args){
+        int[] arr={4,6,8,9,6,3,1};
+
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
---
+++ Program9.java
@@ -0,0 +1,23 @@
+//  Check the Array is sorted
+
+import java.util.Scanner;
+
+public class Program9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        int n =sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int temp=0;
+        for(int i=0;i<n;i++){
+            
+        }
+
+    }
+    
+}
