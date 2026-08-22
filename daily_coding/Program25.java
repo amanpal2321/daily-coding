d3ab7459c22bc76a7ef5642f6502e02cdf1bc68a
Aman Pal <amanpal2321@gmail.com>
2026-08-22 22:34:53 +0530

    frequency of element

---
+++ daily_coding/Program25.java
@@ -0,0 +1,21 @@
+
+ import java.util.HashMap;
+ public class Program25 {
+   
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1, 1, 4};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            System.out.println(key + " -> " + map.get(key));
+        }
+    }
+}
+
