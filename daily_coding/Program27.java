5eeb1043ac1350b2d191d0c205f469cd0528a96a
Aman Pal <amanpal2321@gmail.com>
2026-08-24 21:05:36 +0530

    Encapsulation practice code

---
+++ daily_coding/Program27.java
@@ -0,0 +1,15 @@
+public class Program27 {
+    private int a;
+    private int b;
+    public Program27(int a, int b) {
+        this.a = a;
+        this.b = b;
+
+    }
+    public int getA() {
+        return a ;
+    }
+    public int getB() {
+        return b ;
+    }
+}
---
+++ daily_coding/Program27driver.java
@@ -0,0 +1,8 @@
+public class Program27driver {
+    public static void main(String[] args) {
+        Program27 obj = new Program27(10, 20);
+        System.out.println("Value of a: " + obj.getA());
+        System.out.println("Value of b: " + obj.getB());
+    }
+    
+}
