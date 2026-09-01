41477d9d0ee9545f6734800a1c0a553bb7a9b631
Aman Pal <amanpal2321@gmail.com>
2026-09-01 19:08:40 +0530

    Striver Program 1

---
+++ daily_coding/Striver/LargestElement.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class LargestElement {
+public static void main(String[] args) {
+    System.out.print("Enter array size: ");
+    Scanner sc = new Scanner(System.in);
+     
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    System.out.print("Enter array Element: ");
+    int largest=0;
+    for(int i=0;i<n;i++){
+        arr[i]= sc.nextInt();
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]> largest){
+            largest = arr[i];
+        }
+    }
+    System.out.println("Largest number is: "+largest);
+}
+    }
+
+    
