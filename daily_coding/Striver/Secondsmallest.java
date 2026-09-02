d947e76555a55267f436796c92f6fedf7a51aab7
Aman Pal <amanpal2321@gmail.com>
2026-09-02 12:34:24 +0530

    Second smallest

---
+++ daily_coding/Striver/Secondsmallest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Secondsmallest{
+    public static void main(String[] args) {
+        System.out.print("Enter Array size: ");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter Elements of array: ");
+
+        int smallest = Integer.MAX_VALUE;
+        int secondsmallest = Integer.MAX_VALUE;
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]< smallest){
+                smallest = arr[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]< secondsmallest  && arr[i]!=smallest){
+                secondsmallest = arr[i];
+            }
+        }
+        
+        System.out.println("Smallest Element is : "+secondsmallest);
+
+    }
+}
