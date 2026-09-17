eeaaeef0c475d6d8232d902a90e8cad246982c7c
Aman Pal <amanpal2321@gmail.com>
2026-09-17 23:46:45 +0530

    Best code for perfect no

--- 5PM and sunday DSA/5PM DSA/PerfectNo.java
+++ 5PM and sunday DSA/5PM DSA/PerfectNo.java
@@ -1,11 +1,20 @@
 public class PerfectNo {
-    public static void main(String[] args) {
-        int n =6;
-        if( isPerfect(n));
-        System.out.println(n+"is aPerfect Number");
+    class Solution {
+    public boolean checkPerfectNumber(int n) {
+        if(n<=1)
+        return false;
+        int sum=1;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                int fact1=i, fact2=n/i;
+                if(fact1!=fact2){
+                    sum+=(fact1+fact2);
+                }else{
+                    sum=fact1;
+                }
+            }
+        }
+        return sum==n;
     }
-
-    private static boolean isPerfect(int n) {
-        
-    }
+}
 }
